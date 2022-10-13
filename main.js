b0d31b58e6e7d5f06d1484456487f4ddad04d534
Bill Zhou <zhou.xu@husky.neu.edu>
2022-10-13 19:03:45 -0400

    almost finished

--- main.js
+++ main.js
@@ -13,24 +13,6 @@ function circleClicked(circle) {
 
 }
 
-function addPoint() {
-    X = document.getElementById('x-value');
-    cx = X.options[X.selectedIndex].text;
-
-    Y = document.getElementById('y-value');
-    cy = Y.options[Y.selectedIndex].text;
-
-    var newP = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-    newP.id = "(" + cx + "," + cy + ")";
-    newP.setAttribute('cx', 40*cx + 50);
-    newP.setAttribute('cy', 440 - 40*cy);
-    newP.setAttribute('r', 10);
-    newP.addEventListener("click", circleClicked);
-
-    frame = document.getElementById('frame');
-    frame.appendChild(newP)
-
-}
 
 for (let i = 0; i < circles.length; i++) {
 	circles[i].addEventListener("click", circleClicked);
@@ -83,7 +65,7 @@ d3.csv("data/scatter-data.csv").then((data) => {
             .data(data) // this is passed from  .then()
             .enter()  
             .append("circle")
-            .attr("id", (d) => {return "(" + d.x + "," + d.y + ")"; })
+            .attr("id", (d) => { return "(" + d.x + "," + d.y + ")"; })
             .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left); }) // use x for cx
             .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.top); }) // use y for cy
             .attr("r", 10)  // set r 
@@ -93,16 +75,150 @@ d3.csv("data/scatter-data.csv").then((data) => {
     FRAME1.append("g") 
         .attr("transform", "translate(" + MARGINS.left + 
             "," + (VIS_HEIGHT + MARGINS.top) + ")") 
-        .call(d3.axisBottom(X_SCALE).ticks(4)) 
+        .call(d3.axisBottom(X_SCALE).ticks(5)) 
             .attr("font-size", '20px'); 
 
     // Add an y-axis to the vis  
     FRAME1.append('g')  // g is a general SVG
         .attr('transform', "translate(" + MARGINS.left +
             "," + (MARGINS.bottom) +")") 
-        .call(d3.axisLeft(Y_SCALE).ticks(4))
+        .call(d3.axisLeft(Y_SCALE).ticks(5))
             .attr('font-size', '20px');
 
     FRAME1.selectAll(".circle").on("click", circleClicked);
 
+    function addPoint() {
+        console.log(1);
+        X = document.getElementById('x-value');
+        cx = X.options[X.selectedIndex].text;
+    
+        Y = document.getElementById('y-value');
+        cy = Y.options[Y.selectedIndex].text;
+    
+        // let newPoint = {"x": cx, "y": cy};
+        // let newData = [newPoint];
+    
+        // console.log(newData[0].x)
+    
+        data.push({'x': cx, 'y': cy});
+
+        FRAME1.selectAll('.circle')
+            .remove()
+        FRAME1.selectAll('circle')
+            .data(data) // passed from .then  
+            .enter()      
+            .append("circle")  
+                .attr("id", (d) => { return "(" + d.x + "," + d.y + ")"; })
+                .attr("cx", (d) => { return (X_SCALE(d.x) + MARGINS.left);}) 
+                .attr("cy", (d) => { return (Y_SCALE(d.y) + MARGINS.top);}) 
+                .attr("r", 10)
+                .attr("fill", "blue")
+                .attr("class", "circle")
+                    .on("click", circleClicked);
+    
+    }
+    
+    d3.select("#button").on("click", addPoint);
+
+    
+
 }); // .then is closed here 
+
+
+const VIS_W = FRAME_WIDTH - MARGINS.left - MARGINS.right;
+const VIS_H = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom; 
+
+const FRAME2 = d3.select("#vis2")
+                .append("svg")
+                    .attr("width", FRAME_WIDTH)
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("class", "frame"); 
+
+
+function build_bar_chart() {
+    d3.csv("data/bar-data.csv").then((data) => {
+
+        const MAX_Y = d3.max(data, (d) => { return parseInt(d.amount); });
+
+        // X scale
+        const X_SCALE = d3.scaleBand() // for categorical data 
+                            .range([0, VIS_W])
+                            .domain(data.map(d => d.category))
+                            .padding(0.2); 
+        // Add X axis
+        FRAME2.append("g")
+                .attr("transform", "translate(" + MARGINS.left +
+                    "," + (VIS_H + MARGINS.top) + ")")
+                .call(d3.axisBottom(X_SCALE));  
+
+        // Y scale 
+        const Y_SCALE = d3.scaleLinear()
+                            .domain([0, MAX_Y])
+                            .range([VIS_H, 0]); 
+
+        // Add Y axis 
+        FRAME2.append("g")
+                .attr("transform", "translate(" + MARGINS.left +
+                    "," + MARGINS.top + ")")
+                .call(d3.axisLeft(Y_SCALE));  
+
+        // Add bars 
+        FRAME2.selectAll("bars")
+                .data(data)
+                .enter()
+                .append("rect")
+                    .attr("x", (d) => {
+                        // x pos depends on category 
+                        return (X_SCALE(d.category) + MARGINS.left); 
+                    })
+                    .attr("y", (d) => {
+                        // start of rect depends on value 
+                        return (Y_SCALE(d.amount) + MARGINS.top);
+                    })
+                    .attr("height", (d) => {
+                        // height of bar should be height of vis - value
+                        return (VIS_H - Y_SCALE(d.amount));
+                    })
+                    .attr("width", X_SCALE.bandwidth())
+                    .attr("fill", "blue") // width comes from X_SCALE for free
+                    .attr("class", "bar"); 
+    })
+
+
+    const TOOLTIP = d3.select("#vis2")
+                        .append("div")
+                          .attr("class", "tooltip")
+                          .style("opacity", 0); 
+
+    // Define event handler functions for tooltips
+    function handleMouseover(event, d) {
+      // on mouseover, make opaque 
+      TOOLTIP.style("opacity", 1); 
+      console.log("mouseover");
+      
+    }
+
+    function handleMousemove(event, d) {
+      // position the tooltip and fill in information 
+      TOOLTIP.html("Category: " + d.category + "<br>Amount: " + d.amount)
+              .style("left", (event.pageX + 10) + "px") //add offset
+                                                          // from mouse
+              .style("top", (event.pageY - 50) + "px"); 
+        console.log("mousemove");
+    }
+
+    function handleMouseleave(event, d) {
+      // on mouseleave, make transparant again 
+      TOOLTIP.style("opacity", 0); 
+    } 
+
+    // Add event listeners
+    FRAME2.selectAll(".bar")
+          .on("mouseover", handleMouseover)
+          .on("mousemove", handleMousemove)
+          .on("mouseleave", handleMouseleave);  
+
+
+}
+
+build_bar_chart()
